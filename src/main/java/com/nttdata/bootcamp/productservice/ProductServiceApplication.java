b610e2c6d54d1cc4f998e18dcfe1ea311d747800
Javs8356 <javs8356@gmail.com>
2022-11-23 11:07:23 +0100

    añadida función /port

--- src/main/java/com/nttdata/bootcamp/productservice/ProductServiceApplication.java
+++ src/main/java/com/nttdata/bootcamp/productservice/ProductServiceApplication.java
@@ -1,12 +1,25 @@
 package com.nttdata.bootcamp.productservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class ProductServiceApplication {
+	
+	@Value("${server.port}")
+	private String port;
+	
+	@RequestMapping("/port")
+	  public String Port() {
+	    return "El puerto del micro es " +port;
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
